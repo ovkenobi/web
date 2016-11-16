d7e7aac30699d676dfa4116de7be64e35fb06e4d
ovkenobi <ovkenobi@yandex.ru>
2016-11-16 12:19:11 +0500

    check with gunicorn

--- etc/init.sh
+++ etc/init.sh
@@ -1,4 +1,4 @@
 ln -sf $PWD/nginx.conf /etc/nginx/sites-enabled/default
 /etc/init.d/nginx restart
-ln -sf $PWD/gunicorn.conf /etc/gunicorn.d/test
-/etc/init.d/gunicorn restart
+#ln -sf $PWD/gunicorn.conf /etc/gunicorn.d/test
+#/etc/init.d/gunicorn restart
--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
-def wsgi_application(env, res):
+def app(env, res):
     status = "200 OK"
-    body = '\n'.join(env['QUERY_STRING'].split('&')).encode()
-    headers = [ ("Content-Type", "text/plain")]
+#body = '\n'.join(env['QUERY_STRING'].split('&')).encode()
+    body = "Hello world\n"
+    headers = [ ("Content-Type", "text/plain"), ("Content-Type", str(len(body)))]
     res(status, headers)
     return iter([body])
     
