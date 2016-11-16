4a9aaac3091c2d3666102ed4658461ad8329b078
ovkenobi <ovkenobi@yandex.ru>
2016-11-16 12:54:02 +0500

    bag fix

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
 def app(env, res):
     status = "200 OK"
-#body = '\n'.join(env['QUERY_STRING'].split('&')).encode()
-    body = "Hello world\n"
-    headers = [ ("Content-Type", "text/plain"), ("Content-Type", str(len(body)))]
+    body = '\n'.join(env['QUERY_STRING'].split('&')).encode()
+#    body = "Hello world\n"
+    headers = [ ("Content-Type", "text/plain"), ("Content-Length", str(len(body)))]
     res(status, headers)
     return iter([body])
     
