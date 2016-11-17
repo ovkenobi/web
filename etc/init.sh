a271cddc6bba203fabda55c713cc8e7ed887adb6
ovkenobi <ovkenobi@yandex.ru>
2016-11-17 10:05:02 +0500

    setup django app

--- etc/init.sh
+++ etc/init.sh
@@ -2,3 +2,5 @@ ln -sf $PWD/nginx.conf /etc/nginx/sites-enabled/default
 /etc/init.d/nginx restart
 #ln -sf $PWD/gunicorn.conf /etc/gunicorn.d/test
 #/etc/init.d/gunicorn restart
+
+#gunicorn -b :8000 --pythonpath /home/box/web/ask/ask wsgi:application&
