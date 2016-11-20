961ffe2aadb2ebc7596bd69385383c870141761c
ovkenobi <ovkenobi@yandex.ru>
2016-11-20 16:57:44 +0000

    change models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,17 +2,28 @@ from __future__ import unicode_literals
 from django.db import models                                                    
 from django.contrib.auth.models import User                                     
                                                                                 
-class Question(models.Model):                                                   
+class QuestionManager(models.Manager):
+    def new(): pass
+    def popular(): pass
+
+
+class Question(models.Model):
+    objects = QuestionManager() 
     title = models.CharField(max_length=255)                                    
     text = models.TextField()                                                   
-    added_at = models.DateTimeField(blank=True)                                 
-    rating = models.IntegerField()                                              
-    author = models.ForeignKey(User)                                            
-    likes = models.ManytoManyField(User)                                        
-                                                                                
+    added_at = models.DateTimeField(blank=True, auto_now_add=True) 
+    rating = models.IntegerField(default = 0) 
+    author = models.ForeignKey(User) 
+    likes = models.ManytoManyField(User, through="Likes")
+
+
+class Likes(models.Model):
+    question = models.ForeignKey(Question, related_name="like_question")
+    user = models.ForeignKey(User, related_name="like_user")
                                                                                 
 class Answer(models.Model):                                                     
-    text = models.TextField()                                                   
-    added_at = models.DateTimeField(blank=True)                                 
-    question = models.ForeignKey(Question)                                      
-    author = models.ForeignKey(User)             
+    text = models.TextField() 
+    added_at = models.DateTimeField(blank=True) 
+    question = models.ForeignKey(Question) 
+    author = models.ForeignKey(User)
+
