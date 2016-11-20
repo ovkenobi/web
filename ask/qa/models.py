2bd0741c6c3722adf0ec94f64386fcade9270dd7
ovkenobi <ovkenobi@yandex.ru>
2016-11-20 21:07:54 +0500

    add models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,5 +1,18 @@
-from __future__ import unicode_literals
-
-from django.db import models
-
-# Create your models here.
+from __future__ import unicode_literals                                         
+from django.db import models                                                    
+from django.contrib.auth.models import User                                     
+                                                                                
+class Question(models.Model):                                                   
+    title = models.CharField(max_length=255)                                    
+    text = models.TextField()                                                   
+    added_at = models.DateTimeField(blank=True)                                 
+    rating = models.IntegerField()                                              
+    author = models.ForeignKey(User)                                            
+    likes = models.ManytoManyField(User)                                        
+                                                                                
+                                                                                
+class Answer(models.Model):                                                     
+    text = models.TextField()                                                   
+    added_at = models.DateTimeField(blank=True)                                 
+    question = models.ForeignKey(Question)                                      
+    author = models.ForeignKey(User)             
